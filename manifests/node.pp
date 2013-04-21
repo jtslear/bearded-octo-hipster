7a876b0b0aa06720c5f98277498019cf7968407a
root <root@puppetmaster.(none)>
2013-04-21 12:24:37 -0400

    add git pluging to jenkins, add slave jenkins node

--- manifests/node.pp
+++ manifests/node.pp
@@ -3,4 +3,15 @@ node 'puppetclient1.home' {
 	include java
 	include myrubyapp
 	include jenkins
+	jenkins::plugin {'swarm':}
+	jenkins::plugin {'git':}
+}
+
+node /jenkinsslave.*/ {
+	class  { 'jenkins::slave':
+		ensure => 'enabled',
+		masterurl => 'http://puppetmaster.home:8080',
+		ui_user => 'adminuser',
+		ui_pass => 'adminpass',
+	}
 }
